f07bd4dc6a985d2b300526facce6d937b407853f
j0hnwz <j0hn04v@gmail.com>
2021-03-17 10:52:41 -0300

    first commit

---
+++ Exercício 15/max2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define N 10
+
+int acheMaximo(int vetor[], int inicio, int fim);
+
+int main()
+{
+    int vetor[N];
+    int i;
+
+    for (i = 0; i < N; i++)
+        vetor[i] = rand() % 100;
+
+    printf("Vetor = { ");
+
+    for (i = 0; i < N; i++)
+        printf("%d ", vetor[i]);
+
+    printf("}\n");
+
+    printf("Max: %d", acheMaximo(vetor, 0, N));
+
+    return 0;
+}
+
+int acheMaximo(int vetor[], int inicio, int fim)
+{
+    int max = 0;
+
+    if (fim - inicio == 1)
+    {
+        return vetor[inicio];
+    }
+
+    int meio = (inicio + fim) / 2;
+    int esquerda = acheMaximo(vetor, inicio, meio);
+    int direita = acheMaximo(vetor, meio, fim);
+
+    if (esquerda > direita)
+    {
+        max = esquerda;
+    }
+    else
+    {
+        max = direita;
+    }
+
+    return max;
+}
